f66d7d47372c8c380596e4cee7f62038955efd2c
sumit3645 <154312428+sumit3645@users.noreply.github.com>
2025-06-26 13:39:34 +0530

    Create 01.mutable.py

---
+++ basics/01.mutable.py
@@ -0,0 +1,10 @@
+username="amit"
+username="sumit"
+when we change username from amit to sumit we never change amit, we change the referance username amit to sumit.
+python has automatic garbage collection, and nothing has referance to "amit" so python automatically delete amit.
+immutable means whatever referance has made on memory its never going to be changed, amit never going to be changed
+                         username              memory alloaction=amit
+
+x=10
+datatype(10) hamesha memory me rehta hai, variable me nahi jata hai. variable doesn't have any datatype but memory has
+here x doesn't have any datatype , inside variable a has a number data type
