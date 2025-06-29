2aa3d60fe34d151189486b2bf796eca3a49ebbc2
sumit3645 <154312428+sumit3645@users.noreply.github.com>
2025-06-29 21:25:10 +0530

    Create weather_condition.py

---
+++ basics/problems/weather_condition.py
@@ -0,0 +1,8 @@
+weather=input("what is condition of weather(sunny,rainy,snow): ")
+if weather == "sunny":
+    condition= "go for walk"
+elif weather == "rainy":
+    condition = "read a book"
+else:
+    condition= "watch netflix"
+print(condition)            
