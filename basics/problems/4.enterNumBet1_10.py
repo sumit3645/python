f959b2cb11a58d4ab8f60eb98a97648d94e5beaa
sumit3645 <154312428+sumit3645@users.noreply.github.com>
2025-06-30 11:21:44 +0530

    Create 4.enterNumBet1_10.py

---
+++ basics/problems/4.enterNumBet1_10.py
@@ -0,0 +1,7 @@
+while True:
+    number=int(input("enter number between 1 and 10: "))
+    if 1 <= number <= 10:
+        print("thanks")
+        break
+    else:
+        print("invalid number try again")
